64b93cf83039541719b35d797938fe2a05a00c61
gurolsaglam <gurol_saglam_92@hotmail.com>
2016-03-05 21:28:04 +0200

    quad ultrasonic sensor code optimization

--- Diger/ultrasonic sensor (dörtlü) arduino/quad_ultrasonic_sensor/quad_ultrasonic_sensor.ino
+++ Diger/ultrasonic sensor (dörtlü) arduino/quad_ultrasonic_sensor/quad_ultrasonic_sensor.ino
@@ -10,101 +10,49 @@ Original code improvements to the Ping sketch sourced from Trollmaker.com
 Some code and wiring inspired by http://en.wikiversity.org/wiki/User:Dstaub/robotcar
 */
 
-#define trigPinsouth 6
-#define echoPinsouth 7
-#define trigPinnorth 8
-#define echoPinnorth 9
-#define trigPinwest 10
-#define echoPinwest 11
-#define trigPineast 12
-#define echoPineast 13
+#define trigPinSouth 6
+#define trigPinNorth 8
+#define trigPinWest 10
+#define trigPinEast 12
+
+#define echoPinSouth 7
+#define echoPinNorth 9
+#define echoPinWest 11
+#define echoPinEast 13
 
 
 void setup() {
   Serial.begin (9600);
-  pinMode(trigPineast, OUTPUT);
-  pinMode(echoPineast, INPUT);
-  pinMode(trigPinwest, OUTPUT);
-  pinMode(echoPinwest, INPUT);
-  pinMode(trigPinsouth, OUTPUT);
-  pinMode(echoPinsouth, INPUT);
-  pinMode(trigPinnorth, OUTPUT);
-  pinMode(echoPinnorth, INPUT);
+  
+  pinMode(trigPinEast, OUTPUT);
+  pinMode(trigPinWest, OUTPUT);
+  pinMode(trigPinSouth, OUTPUT);
+  pinMode(trigPinNorth, OUTPUT);
+  
+  pinMode(echoPinEast, INPUT);
+  pinMode(echoPinWest, INPUT);
+  pinMode(echoPinSouth, INPUT);
+  pinMode(echoPinNorth, INPUT);
+}
+
+long calculateDistance(int pinNum, int echoPinNum) {
+  long duration, distance;
+  digitalWrite(pinNum, LOW);  // Added this line
+  delayMicroseconds(2); // Added this line
+  digitalWrite(pinNum, HIGH);
+  delayMicroseconds(10); // Added this line
+  digitalWrite(pinNum, LOW);
+  duration = pulseIn(echoPinNum, HIGH);
+  distance = (duration/2) / 29.1;
+  return distance;
 }
 
 void loop() {
-  long durationeast, distanceeast;
-  digitalWrite(trigPineast, LOW);  // Added this line
-  delayMicroseconds(2); // Added this line
-  digitalWrite(trigPineast, HIGH);
-  delayMicroseconds(10); // Added this line
-  digitalWrite(trigPineast, LOW);
-  durationeast = pulseIn(echoPineast, HIGH);
-  distanceeast = (durationeast/2) / 29.1;
-  /*if (distanceeast >= 200 || distanceeast <= 0){
-    Serial.println("East is out of range");
-    //Serial.println("Out of range");
-  }
-  else {
-    Serial.println("East is " + String(distanceeast));
-    //Serial.print(distanceeast);
-    //Serial.println(" cm");
-  }
-  delay(500); */
-  long durationwest, distancewest;
-  digitalWrite(trigPinwest, LOW);
-  delayMicroseconds(2); // Added this line
-  digitalWrite(trigPinwest, HIGH);
-  delayMicroseconds(10); // Added this line
-  digitalWrite(trigPinwest, LOW);
-  durationwest = pulseIn(echoPinwest, HIGH);
-  distancewest = (durationwest/2) / 29.1;
-  /*if (distancewest >= 200 || distancewest <= 0){
-    Serial.println("West is out of range");
-    //Serial.println("Out of range");
-  }
-  else {
-    Serial.println("West is " + String(distancewest));
-    //Serial.print(distancewest);
-    //Serial.println(" cm");
-  }
-  delay(500); */
-  long durationsouth, distancesouth;
-  digitalWrite(trigPinsouth, LOW);
-  delayMicroseconds(2); // Added this line
-  digitalWrite(trigPinsouth, HIGH);
-  delayMicroseconds(10); // Added this line
-  digitalWrite(trigPinsouth, LOW);
-  durationsouth = pulseIn(echoPinsouth, HIGH);
-  distancesouth = (durationsouth/2) / 29.1;
-  /*if (distancesouth >= 200 || distancesouth <= 0){
-    Serial.println("South is out of range");
-    //Serial.println("Out of range");
-  }
-  else {
-    Serial.println("South is " + String(distancesouth));
-    //Serial.print(distancesouth);
-    //Serial.println(" cm");
-  }
-  delay(500); */
-  long durationnorth, distancenorth;
-  digitalWrite(trigPinnorth, LOW);
-  delayMicroseconds(2); // Added this line
-  digitalWrite(trigPinnorth, HIGH);
-  delayMicroseconds(10); // Added this line
-  digitalWrite(trigPinnorth, LOW);
-  durationnorth = pulseIn(echoPinnorth, HIGH);
-  distancenorth = (durationnorth/2) / 29.1;
-  /*if (distancenorth >= 200 || distancenorth <= 0){
-    Serial.println("North is out of range");
-    //Serial.println("Out of range");
-  }
-  else {
-    Serial.println("North is " + String(distancenorth));
-   // Serial.print(distancenorth);
-   // Serial.println(" cm");
-  }
-  delay(1000); */
-  Serial.println("East is "+String(distanceeast)+", West is "+String(distancewest)+", South is "+String(distancesouth)+", North is "+String(distancenorth));
+  long distanceEast = calculateDistance(trigPinEast, echoPinEast);
+  long distanceWest = calculateDistance(trigPinWest, echoPinWest);
+  long distanceSouth = calculateDistance(trigPinSouth, echoPinSouth);
+  long distanceNorth = calculateDistance(trigPinNorth, echoPinNorth);
+  
+  Serial.println("New: East is "+String(distanceEast)+", West is "+String(distanceWest)+", South is "+String(distanceSouth)+", North is "+String(distanceNorth));
   delay(1000);
 }
