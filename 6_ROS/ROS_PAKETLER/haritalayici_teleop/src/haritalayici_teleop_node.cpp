52c045f87aa45b7edebdc756f86698249ff74e63
researcherOne <birkan.kolcu@ozu.edu.tr>
2016-07-20 00:22:17 +0300

    eklendi raspberry pi 2 pin semasi degistirildi haritalayici_teleop.launch degistirildi harilatayici_teleop_node eklendi third_gpio_control_node

--- 6_ROS/ROS_PAKETLER/haritalayici_teleop/src/haritalayici_teleop_node.cpp
+++ 6_ROS/ROS_PAKETLER/haritalayici_teleop/src/haritalayici_teleop_node.cpp
@@ -12,7 +12,7 @@ ROS_INFO("%d",msg.code);
 std_msgs::String message;
 std::stringstream ss;
 ss << "stop";
-
+message.data = ss.str();
 chatter_pub.publish(message);
 }
 
@@ -22,19 +22,24 @@ std_msgs::String message;
 std::stringstream ss;
 
 int keyCode = msg.code;
-if (keyCode = 274) {
-	ss << "274 basildi";
-} else {
-	ss << "NOOOOO";
+if (keyCode == 274) {
+ss << "move_backward";
+} else if (keyCode == 273){
+ss << "move_forward";
+} else if (keyCode == 275){
+ss << "move_rightward";
+} else if (keyCode == 276){
+ss << "move_leftward";
 }
+message.data = ss.str();
 chatter_pub.publish(message);
 }
 
 int main(int argc, char **argv){
 ros::init(argc, argv, "little_mapper_teleop_node");
 ros::NodeHandle nh;
-ros::Subscriber sub_keydown = nh.subscribe("keydown", 1000, &keyupMessageRecieved);
-ros::Subscriber sub_keyup = nh.subscribe("keyup", 1000, &keydownMessageRecieved);
+ros::Subscriber sub_keydown = nh.subscribe("keydown", 1000, &keydownMessageRecieved);
+ros::Subscriber sub_keyup = nh.subscribe("keyup", 1000, &keyupMessageRecieved);
 chatter_pub = nh.advertise<std_msgs::String>("move",1000);
 ros::spin();
 }
