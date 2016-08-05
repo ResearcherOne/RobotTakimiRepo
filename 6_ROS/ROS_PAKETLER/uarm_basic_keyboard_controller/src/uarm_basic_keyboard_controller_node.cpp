5a3b8c0b343d6a75ad88d6a6dc8ac1d70e69babf
researcher1 <birkan.kolcu@ozu.edu.tr>
2016-08-05 19:30:46 +0000

    degistirildi uarm_basic_controller_node, servo pos limitleri parametrik hale getirildi, kod okunaklik acisindan optimize edildi

--- 6_ROS/ROS_PAKETLER/uarm_basic_keyboard_controller/src/uarm_basic_keyboard_controller_node.cpp
+++ 6_ROS/ROS_PAKETLER/uarm_basic_keyboard_controller/src/uarm_basic_keyboard_controller_node.cpp
@@ -2,6 +2,8 @@
 #include"keyboard/Key.h"
 #include"std_msgs/Int16.h"
 
+ros::NodeHandle nh;
+
 ros::Publisher basePub;
 ros::Publisher verticalPub;
 ros::Publisher horizontalPub;
@@ -12,6 +14,21 @@ ros::Subscriber subKeydown;
 std_msgs::Int16 basePos;
 std_msgs::Int16 verticalPos;
 std_msgs::Int16 horizontalPos;
+
+int publishRate;
+int posIncreaseRate;
+
+int baseMaxPosLimit;
+int verticalMaxPosLimit;
+int horizontalMaxPosLimit;
+
+int baseMinPosLimit;
+int verticalMinPosLimit;
+int horizontalMinPosLimit;
+
+int baseDefaultPos;
+int verticalDefaultPos;
+int horizontalDefaultPos;
 
 ros::Timer baseTimer;
 ros::Timer verticalTimer;
@@ -31,43 +48,43 @@ bool isHorizontalKeyInversed;
 
 void baseMove(const ros::TimerEvent&){
 	if(isBaseIncrease){
-		if(basePos.data<180)
+		if(basePos.data<baseMaxPosLimit)
 			basePos.data++;
 		else
-			basePos.data = 180;
+			basePos.data = baseMaxPosLimit;
 	} else {
-		if (basePos.data>0)
+		if (basePos.data>baseMinPosLimit)
 			basePos.data--;
 		else
-			basePos.data = 0;
+			basePos.data = baseMinPosLimit;
 	}
 }
 void verticalMove(const ros::TimerEvent&){
 	if(isVerticalIncrease){
-		if(verticalPos.data<180)
+		if(verticalPos.data<verticalMaxPosLimit)
                         verticalPos.data++;
                 else
-                        verticalPos.data = 180;
+                        verticalPos.data = verticalMaxPosLimit;
         } else {
-                if (verticalPos.data>0)
+                if (verticalPos.data>verticalMinPosLimit)
                         verticalPos.data--;
                 else
-                        verticalPos.data = 0;
+                        verticalPos.data = verticalMinPosLimit;
         }
 
 }
 void horizontalMove(const ros::TimerEvent&){
 	if(isHorizontalIncrease){
-                if(horizontalPos.data<180)
-                        horizontalPos.data++;
-                else
-                        horizontalPos.data = 180;
-        } else {
-                if (horizontalPos.data>0)
-                        horizontalPos.data--;
-                else
-                        horizontalPos.data = 0;
-        }
+		if(horizontalPos.data<horizontalMaxPosLimit)
+				horizontalPos.data++;
+		else
+				horizontalPos.data = horizontalMaxPosLimit;
+	} else {
+		if (horizontalPos.data>horizontalMinPosLimit)
+				horizontalPos.data--;
+		else
+				horizontalPos.data = horizontalMinPosLimit;
+	}
 }
 
 bool isAnyValidKeyDown()
@@ -87,10 +104,10 @@ if (keyCode == msg.KEY_w || keyCode == msg.KEY_s) {
         horizontalTimer.stop();
 	isHorizontalKeyDown = false;
 } else if (keyCode == msg.KEY_r){
-        basePos.data = 90;
-        verticalPos.data = 90;
-        horizontalPos.data = 90;
-	basePub.publish(basePos);
+        basePos.data = baseDefaultPos;
+        verticalPos.data = verticalDefaultPos;
+        horizontalPos.data = horizontalDefaultPos;
+		basePub.publish(basePos);
         verticalPub.publish(verticalPos);
         horizontalPub.publish(horizontalPos);
 }
@@ -101,10 +118,10 @@ int keyCode = msg.code;
 if (keyCode == msg.KEY_w) {
 	isVerticalIncrease = !isVerticalKeyInversed;
 	isVerticalKeyDown = true;
-        verticalTimer.start();
+	verticalTimer.start();
 } else if (keyCode == msg.KEY_s){
 	isVerticalIncrease = isVerticalKeyInversed;
-        isVerticalKeyDown = true;
+	isVerticalKeyDown = true;
 	verticalTimer.start();
 } else if (keyCode == msg.KEY_a){
 	isBaseIncrease = !isBaseKeyInversed;
@@ -113,65 +130,104 @@ if (keyCode == msg.KEY_w) {
 } else if (keyCode == msg.KEY_d){
 	isBaseIncrease = isBaseKeyInversed;
 	isBaseKeyDown = true;
-        baseTimer.start();
+	baseTimer.start();
 } else if (keyCode == msg.KEY_e){
 	isHorizontalIncrease = !isHorizontalKeyInversed;
 	isHorizontalKeyDown = true;
-        horizontalTimer.start();
+	horizontalTimer.start();
 } else if (keyCode == msg.KEY_q){
 	isHorizontalIncrease = isHorizontalKeyInversed;
 	isHorizontalKeyDown = true;
-        horizontalTimer.start();
+	horizontalTimer.start();
 } 
+}
+
+void setParameters()
+{
+	ros::param::param<int>("~uarmPublishRate",publishRate,5);
+	ros::param::param<int>("~uarmPoseRate",posIncreaseRate ,8);
+
+	ros::param::param<bool>("~isBaseKeyInversed",isBaseKeyInversed ,"false");
+	ros::param::param<bool>("~isVerticalKeyInversed",isVerticalKeyInversed ,"false");
+	ros::param::param<bool>("~isHorizontalKeyInversed",isHorizontalKeyInversed ,"false");
+
+	ros::param::param<int>("~baseMaxPosLimit",baseMaxPosLimit,180);
+	ros::param::param<int>("~verticalMaxPosLimit",verticalMaxPosLimit ,180);
+	ros::param::param<int>("~horizontalMaxPosLimit",horizontalMaxPosLimit,180);
+
+	ros::param::param<int>("~baseMinPosLimit",baseMinPosLimit,0);
+	ros::param::param<int>("~verticalMinPosLimit",verticalMinPosLimit,0);
+	ros::param::param<int>("~horizontalMinPosLimit",horizontalMinPosLimit,0);
+
+	ros::param::param<int>("~baseDefaultPos",baseDefaultPos,90);
+	ros::param::param<int>("~verticalDefaultPos",verticalDefaultPos,90);
+	ros::param::param<int>("~horizontalDefaultPos",horizontalDefaultPos,90);
+}
+
+void informUser()
+{
+	ROS_INFO_STREAM( "publishRate  " << publishRate );
+	ROS_INFO_STREAM( "posIncreaseRate: " << posIncreaseRate );
+
+	ROS_INFO_STREAM( "isBaseInversed  " << isBaseKeyInversed );
+	ROS_INFO_STREAM( "isVerticalKeyInversed " << isVerticalKeyInversed );
+	ROS_INFO_STREAM( "isHorizontalKeyInversed " << isHorizontalKeyInversed );
+
+	ROS_INFO_STREAM( "baseMaxPosLimit  " << baseMaxPosLimit );
+	ROS_INFO_STREAM( "verticalMaxPosLimit: " << verticalMaxPosLimit );
+	ROS_INFO_STREAM( "horizontalMaxPosLimit " << horizontalMaxPosLimit );
+
+	ROS_INFO_STREAM( "baseMinPosLimit  " << baseMinPosLimit );
+	ROS_INFO_STREAM( "verticalMinPosLimit: " << verticalMinPosLimit );
+	ROS_INFO_STREAM( "horizontalMinPosLimit " << horizontalMinPosLimit );
+
+	ROS_INFO_STREAM( "baseDefaultPos  " << baseDefaultPos );
+	ROS_INFO_STREAM( "verticalDefaultPos: " << verticalDefaultPos );
+	ROS_INFO_STREAM( "horizontalDefaultPos " << horizontalDefaultPos );
+}
+
+void setTimers()
+{
+	baseTimer = nh.createTimer(ros::Duration(1/posIncreaseRate), &baseMove);
+	baseTimer.stop();
+	verticalTimer = nh.createTimer(ros::Duration(1/posIncreaseRate), &verticalMove);
+	verticalTimer.stop();
+	horizontalTimer = nh.createTimer(ros::Duration(1/posIncreaseRate),&horizontalMove);
+	horizontalTimer.stop();
+}
+
+void setSubscribers()
+{
+	ros::Subscriber subKeydown = nh.subscribe("keydown", 1000, &keydownMessageRecieved);
+	ros::Subscriber subKeyup = nh.subscribe("keyup", 1000, &keyupMessageRecieved);
+}
+
+void setPublishers()
+{
+	basePub = nh.advertise<std_msgs::Int16>("base",1000);
+	verticalPub = nh.advertise<std_msgs::Int16>("vertical",1000);
+	horizontalPub = nh.advertise<std_msgs::Int16>("horizontal",1000);
 }
 
 int main(int argc, char **argv){
 ros::init(argc, argv, "uarm_basic_keyboard_controller_node");
-ros::NodeHandle nh;
 
-int publishRate;
-int posIncreaseRate;
+setParameters();
+setSubscribers();
+setPublishers();
+setTimers();
 
-ros::param::param<int>("~uarmPublishRate",publishRate,5);
-ros::param::param<int>("~uarmPoseRate",posIncreaseRate ,8);
-ros::param::param<bool>("~isBaseKeyInversed",isBaseKeyInversed ,"false");
-ros::param::param<bool>("~isVerticalKeyInversed",isVerticalKeyInversed ,"false");
-ros::param::param<bool>("~isHorizontalKeyInversed",isHorizontalKeyInversed ,"false");
-ROS_INFO_STREAM( "publishRate  " << publishRate );
-ROS_INFO_STREAM( "posIncreaseRate: " << posIncreaseRate );
-ROS_INFO_STREAM( "isBaseInversed  " << isBaseKeyInversed );
-ROS_INFO_STREAM( "isVerticalKeyInversed " << isVerticalKeyInversed );
-ROS_INFO_STREAM( "isHorizontalKeyInversed " << isHorizontalKeyInversed );
+informUser();
 
-
-basePos.data = 90;
-verticalPos.data = 90;
-horizontalPos.data = 90;
-
-ros::Subscriber subKeydown = nh.subscribe("keydown", 1000, &keydownMessageRecieved);
-ros::Subscriber subKeyup = nh.subscribe("keyup", 1000, &keyupMessageRecieved);
-
-basePub = nh.advertise<std_msgs::Int16>("base",1000);
-verticalPub = nh.advertise<std_msgs::Int16>("vertical",1000);
-horizontalPub = nh.advertise<std_msgs::Int16>("horizontal",1000);
-
-baseTimer = nh.createTimer(ros::Duration(1/posIncreaseRate), &baseMove);
-baseTimer.stop();
-verticalTimer = nh.createTimer(ros::Duration(1/posIncreaseRate), &verticalMove);
-verticalTimer.stop();
-horizontalTimer = nh.createTimer(ros::Duration(1/posIncreaseRate),&horizontalMove);
-horizontalTimer.stop();
 ros::Rate loop_rate(publishRate);
 while (ros::ok())
 {
 if (isAnyValidKeyDown()){
-	basePub.publish(basePos);
-	verticalPub.publish(verticalPos);
-	horizontalPub.publish(horizontalPos);	
+	basePub.publish(baseDefaultPos);
+	verticalPub.publish(verticalDefaultPos);
+	horizontalPub.publish(horizontalDefaultPos);	
 }
 ros::spinOnce();
 loop_rate.sleep();
 }
 }
-
-
