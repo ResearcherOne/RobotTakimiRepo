352ea0254c51a2b773b1e2772533c1ab2207f5f8
birkan kolcu <ResearcherOne@users.noreply.github.com>
2016-11-11 14:38:56 +0200

    little_mapper_teleop_node.cpp refactored
    refactored indentation levels

--- 6_ROS/ROS_PAKETLER/minimal_package/little_mapper_teleop/src/little_mapper_teleop_node.cpp
+++ 6_ROS/ROS_PAKETLER/minimal_package/little_mapper_teleop/src/little_mapper_teleop_node.cpp
@@ -11,16 +11,16 @@ std::string intToString(int number){
 }
 
 void keyupMessageRecieved(const keyboard::Key& msg){
-ROS_INFO("%s",intToString(msg.code).c_str());
+	ROS_INFO("%s",intToString(msg.code).c_str());
 }
 void keydownMessageRecieved(const keyboard::Key& msg){
-ROS_INFO("%s",intToString(msg.code).c_str());
+	ROS_INFO("%s",intToString(msg.code).c_str());
 }
 
 int main(int argc, char **argv){
-ros::init(argc, argv, "little_mapper_teleop_node");
-ros::NodeHandle nh;
-ros::Subscriber sub_keydown = nh.subscribe("keyboard_node/keydown", 1000, &keyupMessageRecieved);
-ros::Subscriber sub_keyup = nh.subscribe("keyboard_node/keyup", 1000, &keydownMessageRecieved);
-ros::spin();
+	ros::init(argc, argv, "little_mapper_teleop_node");
+	ros::NodeHandle nh;
+	ros::Subscriber sub_keydown = nh.subscribe("keyboard_node/keydown", 1000, &keyupMessageRecieved);
+	ros::Subscriber sub_keyup = nh.subscribe("keyboard_node/keyup", 1000, &keydownMessageRecieved);
+	ros::spin();
 }
