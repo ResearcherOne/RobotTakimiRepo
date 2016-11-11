eef9d9e77baa6db4672ad67b21c3c78de1d2a4f3
birkan kolcu <ResearcherOne@users.noreply.github.com>
2016-11-11 14:36:55 +0200

    haritalayici_teleop_node.cpp refactoring
    refactored indentation levels

--- 6_ROS/ROS_PAKETLER/haritalayici_teleop/src/haritalayici_teleop_node.cpp
+++ 6_ROS/ROS_PAKETLER/haritalayici_teleop/src/haritalayici_teleop_node.cpp
@@ -7,39 +7,39 @@
 ros::Publisher chatter_pub;
 
 void keyupMessageRecieved(const keyboard::Key& msg){
-ROS_INFO("%d",msg.code);
+  ROS_INFO("%d",msg.code);
 
-std_msgs::String message;
-std::stringstream ss;
-ss << "stop";
-message.data = ss.str();
-chatter_pub.publish(message);
+  std_msgs::String message;
+  std::stringstream ss;
+  ss << "stop";
+  message.data = ss.str();
+  chatter_pub.publish(message);
 }
 
 
 void keydownMessageRecieved(const keyboard::Key& msg){
-std_msgs::String message;
-std::stringstream ss;
+  std_msgs::String message;
+  std::stringstream ss;
 
-int keyCode = msg.code;
-if (keyCode == 274) {
-ss << "move_backward";
-} else if (keyCode == 273){
-ss << "move_forward";
-} else if (keyCode == 275){
-ss << "move_rightward";
-} else if (keyCode == 276){
-ss << "move_leftward";
-}
-message.data = ss.str();
-chatter_pub.publish(message);
+  int keyCode = msg.code;
+  if (keyCode == 274) {
+    ss << "move_backward";
+  } else if (keyCode == 273){
+    ss << "move_forward";
+  } else if (keyCode == 275){
+    ss << "move_rightward";
+  } else if (keyCode == 276){
+    ss << "move_leftward";
+  }
+  message.data = ss.str();
+  chatter_pub.publish(message);
 }
 
 int main(int argc, char **argv){
-ros::init(argc, argv, "little_mapper_teleop_node");
-ros::NodeHandle nh;
-ros::Subscriber sub_keydown = nh.subscribe("keydown", 1000, &keydownMessageRecieved);
-ros::Subscriber sub_keyup = nh.subscribe("keyup", 1000, &keyupMessageRecieved);
-chatter_pub = nh.advertise<std_msgs::String>("move",1000);
-ros::spin();
+  ros::init(argc, argv, "little_mapper_teleop_node");
+  ros::NodeHandle nh;
+  ros::Subscriber sub_keydown = nh.subscribe("keydown", 1000, &keydownMessageRecieved);
+  ros::Subscriber sub_keyup = nh.subscribe("keyup", 1000, &keyupMessageRecieved);
+  chatter_pub = nh.advertise<std_msgs::String>("move",1000);
+  ros::spin();
 }
